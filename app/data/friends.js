e9d0b9733c06bf0cefc1fab158ea84e0b7515356
Jose Bautista <pierobautista@gmail.com>
2018-05-22 16:40:09 -0400

    final

---
+++ app/data/friends.js
@@ -0,0 +1,52 @@
+//friends data
+var friendsArray = [
+	{
+		name: 'BOB',
+		photo: 'https://popupplayvillage.co.uk/wp-content/uploads/2018/03/Bob-the-builder.jpg',
+		scores: [
+			'1',
+			'3',
+			'3',
+			'3',
+			'5',
+			'5',
+			'5',
+			'3',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Sponge Bob',
+		photo: 'https://vignette.wikia.nocookie.net/spongebob/images/e/e4/SpongeBob_%285%29.png/revision/latest?cb=20140824163949',
+		scores: [
+			'2',
+			'4',
+			'2',
+			'2',
+			'4',
+			'4',
+			'4',
+			'2',
+			'4',
+			'5'
+		]
+	},
+	{
+		name: 'Buzz',
+		photo: 'https://lumiere-a.akamaihd.net/v1/images/open-uri20150422-20810-a07syh_9331bd0a.jpeg?region=0,0,450,450',
+		scores: [
+			'4',
+			'3',
+			'4',
+			'1',
+			'3',
+			'4',
+			'4',
+			'3',
+			'1',
+			'1'
+		]
+	},
+];
+module.exports = friendsArray;
